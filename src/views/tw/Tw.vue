a2761c89aafe0a79637c4569f29f08decb312eb3
hschae <openaid.chae@gmail.com>
2023-10-11 16:56:29 +0900

    fix: tw fix

--- src/views/tw/Tw.vue
+++ src/views/tw/Tw.vue
@@ -1,5 +1,8 @@
 <script setup>
-import '@/assets/tw.css'
+/*
+css 파일을 임포트하면 홈 화면으로 되돌아가도 스타일이 남아있어서 @tailwind 구문을 파일이 아닌 현재 스코프로 제한하여 적용
+*/
+// import '@/assets/tw.css'
 </script>
 
 <template>
@@ -7,5 +10,7 @@ import '@/assets/tw.css'
 </template>
 
 <style scoped>
-
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
